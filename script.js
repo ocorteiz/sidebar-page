f2eab59c81ad1a9c5d0b831be33b028c4acd30a5
Luis Cortêz <112598652+ocorteiz@users.noreply.github.com>
2023-09-02 20:45:39 -0300

    att

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const stBtn = document.querySelector('.st-btn')
-const sideBar = document.querySelector('.sidebar')
+const itemMenu = document.querySelectorAll('.item-menu')
 
 stBtn.addEventListener('click', () => {
     document.body.classList.toggle('heave')
+})
+
+function selectLink() {
+    itemMenu.forEach((item) => {
+        item.classList.remove('ativo')
+    })
+    this.classList.add('ativo')
+}
+
+itemMenu.forEach((item) => {
+    item.addEventListener('click', selectLink)
 })
