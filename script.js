7d471ba31e69807ffd4b22686f4e34992d721713
Luis Cortêz <112598652+ocorteiz@users.noreply.github.com>
2023-09-02 20:20:33 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const stBtn = document.querySelector('.st-btn')
+const sideBar = document.querySelector('.sidebar')
+
+stBtn.addEventListener('click', () => {
+    document.body.classList.toggle('heave')
+})
